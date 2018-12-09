01ebbb3a9fbc57a8f986418bf4a1377346835293
Shane Dunne <shane@getdunne.com>
2018-12-09 16:24:49 -0500

    Update to JUCE 5.4.1, improve colours

--- Source/CCSlider.cpp
+++ Source/CCSlider.cpp
@@ -3,18 +3,19 @@
 // https://www.webnots.com/vibgyor-rainbow-color-codes/
 static Colour rainbow[] =
 {
-    Colour(0xFF9400D3),
-    Colour(0xFF4B0082),
-    Colour(0xFF0000FF),
-    Colour(0xFF00FF00),
-    Colour(0xFFFFFF00),
-    Colour(0xFFFF7F00),
-    Colour(0xFFFF0000),
+    Colour(0xFFB450E3),     // purple
+    Colour(0xFF5B4092),
+    Colour(0xFF4F3FFF),     // blue
+    Colour(0xFF00FF00),     // green
+    Colour(0xFFFFFF00),     // yellow
+    Colour(0xFFFF7F00),     // dark orange
+    Colour(0xFFFF0000),     // red
 };
 
 void CCSlider::paint(Graphics &g, int width, int height, bool rowIsSelected)
 {
     int rainbowIndex = ccNumber % (sizeof(rainbow) / sizeof(Colour));
+    DBG("ccNumber " + String(ccNumber) + " rainbowIndex " + String(rainbowIndex));
 
     g.setColour(rowIsSelected ? Colours::hotpink : Colours::lightgrey);
     Rectangle<int> area(0, 0, width, height);
